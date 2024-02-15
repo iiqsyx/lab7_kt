043edc714db212cfb87ca13999bf5bf04917b506
iiqsyx <alex_iiqsyx@mail.ru>
2024-02-15 18:46:19 +0300

    feat: floatingActionButton added

--- app/src/main/java/ui/screens/NotesScreen.kt
+++ app/src/main/java/ui/screens/NotesScreen.kt
@@ -3,9 +3,14 @@ package ui.screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -51,6 +56,21 @@ fun NotesScreen(
                }
            )
        },
+
+       floatingActionButtonPosition = FabPosition.End,
+       floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { viewModel.onCreateNewNoteClick() },
+                    contentColor = MaterialTheme.colors.background,
+                    content = {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "Add Note Button"
+                        )
+                    }
+                )
+       },
+
        content = {
            if (notes.isNotEmpty()) {
                NotesList(
