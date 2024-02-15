de554886fa5e58e0b9581ac0dc81f2f1803599f6
iiqsyx <alex_iiqsyx@mail.ru>
2024-02-15 18:22:53 +0300

    feat: fiels have been added

---
+++ app/src/main/java/ui/screens/NotesScreen.kt
@@ -0,0 +1,74 @@
+package ui.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.tooling.preview.Preview
+import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.viewmodel.MainViewModel
+import ui.components.Note
+import ui.components.TopAppBar
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun NotesScreen(
+    viewModel: MainViewModel
+){
+    val notes: List<NoteModel> by viewModel
+        .notesNotInTrash
+        .observeAsState(listOf())
+
+   Scaffold (
+        topBar = {TopAppBar(title = "Заметки" ,
+        icon = Icons.Filled.List,
+        onIconClick = {}
+        )},
+       content = {
+           if (notes.isNotEmpty()) {
+               NotesList(
+                   notes = notes, onNoteCheckedChange = {
+                       viewModel.onNoteCheckedChange(it)
+                   },
+                   onNoteClick = { viewModel.onNoteClick(it)}
+               )
+           }
+       }
+   )
+}
+
+@Composable
+private fun NotesList(
+    notes: List<NoteModel>,
+    onNoteCheckedChange: (NoteModel) -> Unit,
+    onNoteClick:(NoteModel) -> Unit
+){
+    LazyColumn{
+        items(count = notes.size){noteIndex ->
+            val note = notes[noteIndex]
+            Note(note = note,
+                onNoteClick = onNoteClick,
+                onNoteCheckedChange = onNoteCheckedChange
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun  NotesListPreview(){
+    NotesList(
+        notes = listOf(
+            NoteModel(1,"Note 1", "Content 1", null),
+            NoteModel(2,"Note 2", "Content 2", false) ,
+            NoteModel(3,"Note 1", "Content 3", true)
+        ),
+        onNoteCheckedChange = {},
+        onNoteClick = {}
+    )
+}
